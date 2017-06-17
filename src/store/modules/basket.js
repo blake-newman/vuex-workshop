e190631906844739757b628f3cc8394f23cc0eb9
Blake Newman <blake.newman@askattest.com>
2017-06-17 20:29:27 +0100

    Add actions
    - Add `product/get` action
    - Add `product/getById` action
    - Add `basket/get` action
    - Add `basket/purchase` action
    - Add `basket/addProduct` action
    - Add `basket/updateQuantity` action
    - Add `basket/removeProduct` action

--- src/store/modules/basket.js
+++ src/store/modules/basket.js
@@ -1,4 +1,11 @@
 import Vue from 'vue'
+import {
+  getBasket,
+  purchaseBasket,
+  addProductToBasket,
+  updateProductInBasket,
+  removeProductFromBasket,
+} from '../../api/basket'
 
 function createDefaultState() {
   return {}
@@ -18,8 +25,50 @@ const mutations = {
   },
 }
 
+const actions = {
+  async get({ commit, dispatch, rootState }) {
+    const response = await getBasket()
+    const { payload } = response.data
+    const ids = payload.map(({ id }) => id)
+    await Promise.all(ids.map(id => dispatch('product/getById', { id }, { root: true })))
+    commit('ADD', payload)
+    return response
+  },
+
+  async purchase({ commit }) {
+    const response = await purchaseBasket()
+    commit('DESTROY')
+    return response
+  },
+
+  async addProduct({ commit }, { id }) {
+    const response = await addProductToBasket(id)
+    const { payload } = response.data
+    commit('ADD', payload)
+    return response
+  },
+
+  async updateQuantity({ commit }, { id, quantity }) {
+    const response = await updateProductInBasket(id, quantity)
+    const { payload } = response.data
+    commit('ADD', payload)
+    return response
+  },
+
+  async removeProduct({ commit, state }, { id }) {
+    const response = await removeProductFromBasket(id)
+    const { payload } = response.data
+    const ids = payload.map(({ id }) => id)
+    const missing = Object.keys(state).filter(id => !ids.includes(id))
+    commit('REMOVE', missing)
+    commit('ADD', payload)
+    return response
+  },
+}
+
 export const basketModule = {
   namespaced: true,
   state: createDefaultState,
   mutations,
+  actions,
 }
--- src/store/modules/product.js
+++ src/store/modules/product.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import { getProducts, getProductById } from '../../api/product'
 
 function createDefaultState() {
   return {}
@@ -10,8 +11,25 @@ const mutations = {
   },
 }
 
+const actions = {
+  async get({ commit }) {
+    const response = await getProducts()
+    const { payload } = response.data
+    commit('ADD', payload)
+    return response
+  },
+
+  async getById({ commit }, { id }) {
+    const response = await getProductById(id)
+    const { payload } = response.data
+    commit('ADD', [payload])
+    return response
+  },
+}
+
 export const productModule = {
   namespaced: true,
   state: createDefaultState,
   mutations,
+  actions,
 }
