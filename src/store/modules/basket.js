c0fda6f5ecab9c6dced1e4ac45e72a3b25721ccc
Blake Newman <blake.newman@askattest.com>
2017-06-17 19:30:02 +0100

    Add mutations
    - Add `product/ADD` mutation
    - Add `basket/ADD` mutation
    - Add `basket/REMOVE` mutation
    - Add `basket/DESTORY` mutation

--- src/store/modules/basket.js
+++ src/store/modules/basket.js
@@ -1,9 +1,25 @@
+import Vue from 'vue'
+
 function createDefaultState() {
   return {}
 }
 
+const mutations = {
+  ADD(state, payload) {
+    payload.forEach(item => Vue.set(state, item.id, item))
+  },
+
+  REMOVE(state, payload) {
+    payload.forEach(id => Vue.delete(state, id))
+  },
+
+  DESTROY(state) {
+    state = createDefaultState()
+  },
+}
+
 export const basketModule = {
   namespaced: true,
-
   state: createDefaultState,
+  mutations,
 }
--- src/store/modules/product.js
+++ src/store/modules/product.js
@@ -1,9 +1,17 @@
+import Vue from 'vue'
+
 function createDefaultState() {
   return {}
 }
 
+const mutations = {
+  ADD(state, payload) {
+    payload.forEach(item => Vue.set(state, item.id, item))
+  },
+}
+
 export const productModule = {
   namespaced: true,
-
   state: createDefaultState,
+  mutations,
 }
