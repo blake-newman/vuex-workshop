439047c5e9c8ad846bf91f1dc98e4ac61df23a9b
Blake Newman <blake.newman@askattest.com>
2017-06-18 11:00:00 +0100

    Add client fetch resolving
    - Add resoling of fetch methods for client

--- src/entry/client.js
+++ src/entry/client.js
@@ -4,11 +4,27 @@ import { createApp } from './app'
 // Assign to window for libaries to use.
 if (!window.Promise) window.Promise = Promise
 
-const { app, router } = createApp()
+const { app, router, store } = createApp()
 
-// wait until router has resolved all async before hooks
-// and async components...
 router.onReady(() => {
+  router.beforeResolve(async (to, from, next) => {
+    const matched = router.getMatchedComponents(to)
+    const prevMatched = router.getMatchedComponents(from)
+
+    let diffed = false
+    const activated = matched.filter(
+      (c, i) => diffed || (diffed = prevMatched[i] !== c || !!c.refetch),
+    )
+
+    const hooks = activated.map(c => c.fetch).filter(fetch => !!fetch)
+
+    if (hooks.length) {
+      await Promise.all(hooks.map(hook => hook && hook(store, to, from)))
+    }
+
+    next()
+  })
+
   // Actually mount to DOM
   app.$mount('#app')
 })
